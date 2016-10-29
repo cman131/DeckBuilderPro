27b1196a2be81734e75516532f98f347a1e8ad91
Conor Wright <conor13@msn.com>
2016-10-29 12:26:59 -0400

    added popovers for cards

--- app/static/js/collectionmanager.js
+++ app/static/js/collectionmanager.js
@@ -1,3 +1,17 @@
-app.Directive("A", [], function(){
-
+(function(){
+  'use strict';
+  angular.Module('MainApp').Directive("card-popover", function(){
+    return {
+      restrict: "A",
+      scope: {
+        popoverCard: "=cardPopover"
+      },
+      link: function() {
+        element.popover({
+          trigger: 'hover',
+          templateUrl: './static/card_popover.html'
+        });
+      }
+    };
+  });
 });
